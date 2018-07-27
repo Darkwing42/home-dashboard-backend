d0ac93bdd6b60f353cb67a1f947b802bf7682132
Darkwing42 <jeanmarc.wagner@gmx.de>
2018-07-27 11:19:44 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO
 import os, sys
 
 
 app = Flaske(__name__)
 io = SocketIO(app)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template("index.html")
 
 
 @io.on('connect')
