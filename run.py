df203c0030fbc8bd3bc46ea888d6a695f0cafc3f
Darkwing42 <jeanmarc.wagner@gmx.de>
2018-07-27 10:53:02 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,17 @@
+from flask import Flask
+from flask_socketio import SocketIO
+import os, sys
 
+
+app = Flaske(__name__)
+
+io = SocketIO(app)
+
+@io.on('connect')
+def connect():
+	print('Client connected')
+	send({'message': 'Client connected',
+		  'connection': true }
+		 
+if __name__ == '__main__':
+	io.run(app)
